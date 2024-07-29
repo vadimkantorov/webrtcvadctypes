0f54c5591fc4e4166a4a48f79b705f017b563fda
Vadim Kantorov <vadimkantorov@gmail.com>
2024-07-30 01:16:22 +0300

    ...

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-SOURCES = \
+SOURCESGMM = \
 src/common_audio/signal_processing/complex_bit_reverse.c \
 src/common_audio/signal_processing/complex_fft.c \
 src/common_audio/signal_processing/cross_correlation.c \
@@ -23,7 +23,7 @@ src/common_audio/vad/vad_sp.c \
 src/common_audio/vad/webrtc_vad.c \
 src/rtc_base/checks.cc
 
-INCLUDES = \
+INCLUDESGMM = \
 src/common_audio/signal_processing/complex_fft_tables.h \
 src/common_audio/signal_processing/dot_product_with_scale.h \
 src/common_audio/signal_processing/include/real_fft.h \
@@ -45,5 +45,80 @@ src/rtc_base/system/inline.h \
 src/rtc_base/type_traits.h \
 src/system_wrappers/include/cpu_features_wrapper.h
 
+SOURCESRNN = \
+src/common_audio/resampler/push_sinc_resampler.cc \
+src/common_audio/resampler/sinc_resampler.cc \
+src/common_audio/wav_file.cc \
+src/rtc_base/checks.cc \
+src/rtc_base/logging.cc \
+src/rtc_base/string_utils.cc \
+src/rtc_base/time_utils.cc \
+src/rtc_base/string_encode.cc \
+src/rtc_base/platform_thread_types.cc \
+src/rtc_base/system/file_wrapper.cc \
+src/rtc_base/strings/string_builder.cc \
+src/common_audio/wav_header.cc \
+src/common_audio/audio_util.cc \
+src/modules/audio_processing/agc2/rnn_vad/features_extraction.cc \
+src/modules/audio_processing/agc2/rnn_vad/rnn.cc \
+src/modules/audio_processing/agc2/biquad_filter.cc \
+src/rtc_base/memory/aligned_malloc.cc \
+src/modules/audio_processing/agc2/rnn_vad/pitch_search.cc \
+src/modules/audio_processing/agc2/rnn_vad/lp_residual.cc \
+src/modules/audio_processing/agc2/rnn_vad/auto_correlation.cc \
+src/modules/audio_processing/agc2/rnn_vad/pitch_search_internal.cc \
+src/modules/audio_processing/agc2/rnn_vad/spectral_features.cc \
+src/modules/audio_processing/agc2/rnn_vad/spectral_features_internal.cc \
+src/modules/audio_processing/utility/pffft_wrapper.cc \
+third_party/rnnoise/src/rnn_vad_weights.cc \
+third_party/pffft/src/pffft.c \
+third_party/pffft/src/fftpack.c
+
+INCLUDESRNN = \
+src/common_audio/resampler/push_sinc_resampler.h \
+src/common_audio/resampler/sinc_resampler.h \
+src/common_audio/wav_file.h \
+src/rtc_base/checks.h \
+src/rtc_base/logging.h \
+src/rtc_base/string_utils.h \
+src/rtc_base/time_utils.h \
+src/rtc_base/atomic_ops.h \
+src/rtc_base/sanitizer.h \
+src/rtc_base/string_encode.h \
+src/rtc_base/thread_annotations.h \
+src/rtc_base/platform_thread_types.h \
+src/rtc_base/numerics/safe_minmax.h \
+src/rtc_base/system/file_wrapper.h \
+src/rtc_base/system/unused.h \
+src/rtc_base/numerics/safe_conversions.h \
+src/rtc_base/numerics/safe_conversions_impl.h \
+src/rtc_base/strings/string_builder.h \
+src/common_audio/wav_header.h \
+src/common_audio/include/audio_util.h \
+src/modules/audio_processing/agc2/rnn_vad/features_extraction.h \
+src/modules/audio_processing/agc2/rnn_vad/common.h \
+src/modules/audio_processing/agc2/rnn_vad/rnn.h \
+src/modules/audio_processing/agc2/biquad_filter.h \
+src/rtc_base/memory/aligned_malloc.h \
+src/modules/audio_processing/agc2/rnn_vad/pitch_info.h \
+src/modules/audio_processing/agc2/rnn_vad/pitch_search.h \
+src/modules/audio_processing/agc2/rnn_vad/sequence_buffer.h \
+src/modules/audio_processing/agc2/rnn_vad/lp_residual.h \
+src/modules/audio_processing/agc2/rnn_vad/auto_correlation.h \
+src/modules/audio_processing/agc2/rnn_vad/pitch_search_internal.h \
+src/modules/audio_processing/agc2/rnn_vad/spectral_features.h \
+src/modules/audio_processing/agc2/rnn_vad/spectral_features_internal.h \
+src/modules/audio_processing/agc2/rnn_vad/ring_buffer.h \
+src/modules/audio_processing/agc2/rnn_vad/symmetric_matrix_buffer.h \
+src/modules/audio_processing/utility/pffft_wrapper.h \
+third_party/rnnoise/src/rnn_activations.h \
+third_party/rnnoise/src/rnn_vad_weights.h \
+third_party/pffft/src/pffft.h \
+third_party/pffft/src/fftpack.h
+
+
 webrtcvadctypes.so:
-	$(CXX) $(SOURCES) -DWEBRTC_POSIX -Isrc -Iabseil-cpp -shared -fPIC -o $@
+	$(CXX) $(SOURCESGMM) -DWEBRTC_POSIX -Isrc -Iabseil-cpp -shared -fPIC -o $@
+
+rnn.so:
+	$(CXX) $(SOURCESRNN) -DWEBRTC_POSIX  -DWEBRTC_LINUX -DNOMINMAX  -Isrc -Iabseil-cpp -I. -shared -fPIC -o $@
--- webrtcvadctypes.py
+++ webrtcvadctypes.py
@@ -6,80 +6,116 @@ import ctypes
 # https://github.com/wiseman/py-webrtcvad/blob/master/example.py
 # https://github.com/vadimkantorov/readaudio/blob/master/decode_audio.py
 
-# src/common_audio/vad/include/webrtc_vad.h
-lib_path = os.path.abspath('webrtcvadctypes.so')
-lib = ctypes.CDLL(lib_path)
+# src/common_audio/vad/webrtc_vad.c
+# src/common_audio/vad/vad.cc
 
-## Creates an instance to the VAD structure.
-#VadInst* WebRtcVad_Create(void);
-lib.WebRtcVad_Create.argtypes = []
-lib.WebRtcVad_Create.restype = ctypes.c_void_p
+class Vad(ctypes.c_void_p):
+    @staticmethod
+    def ffi(lib_path = os.path.abspath('webrtcvadctypes.so')):
+        lib = ctypes.CDLL(lib_path)
+        
+        # src/common_audio/vad/include/webrtc_vad.h
 
+        ## Creates an instance to the VAD structure.
+        #VadInst* WebRtcVad_Create(void);
+        lib.WebRtcVad_Create.argtypes = []
+        lib.WebRtcVad_Create.restype = ctypes.c_void_p
+        
+        
+        ## Frees the dynamic memory of a specified VAD instance.
+        ##
+        ## - handle [i] : Pointer to VAD instance that should be freed.
+        #void WebRtcVad_Free(VadInst* handle);
+        lib.WebRtcVad_Free.argtypes = [ctypes.c_void_p]
+        lib.WebRtcVad_Free.restype = None
+        
+        ## Initializes a VAD instance.
+        ##
+        ## - handle [i/o] : Instance that should be initialized.
+        ##
+        ## returns        : 0 - (OK),
+        ##                 -1 - (null pointer or Default mode could not be set).
+        #int WebRtcVad_Init(VadInst* handle);
+        lib.WebRtcVad_Init.argtypes = [ctypes.c_void_p]
+        lib.WebRtcVad_Init.restype = ctypes.c_int
+        
+        
+        ## Sets the VAD operating mode. A more aggressive (higher mode) VAD is more
+        ## restrictive in reporting speech. Put in other words the probability of being
+        ## speech when the VAD returns 1 is increased with increasing mode. As a
+        ## consequence also the missed detection rate goes up.
+        ##
+        ## - handle [i/o] : VAD instance.
+        ## - mode   [i]   : Aggressiveness mode (0, 1, 2, or 3).
+        ##
+        ## returns        : 0 - (OK),
+        ##                 -1 - (null pointer, mode could not be set or the VAD instance
+        ##                       has not been initialized).
+        #int WebRtcVad_set_mode(VadInst* handle, int mode);
+        lib.WebRtcVad_set_mode.argtypes = [ctypes.c_void_p, ctypes.c_int]
+        lib.WebRtcVad_set_mode.restype = ctypes.c_int
+        
+        ## Calculates a VAD decision for the `audio_frame`. For valid sampling rates
+        ## frame lengths, see the description of WebRtcVad_ValidRatesAndFrameLengths().
+        ##
+        ## - handle       [i/o] : VAD Instance. Needs to be initialized by
+        ##                        WebRtcVad_Init() before call.
+        ## - fs           [i]   : Sampling frequency (Hz): 8000, 16000, or 32000
+        ## - audio_frame  [i]   : Audio frame buffer.
+        ## - frame_length [i]   : Length of audio frame buffer in number of samples.
+        ##
+        ## returns              : 1 - (Active Voice),
+        ##                        0 - (Non-active Voice),
+        ##                       -1 - (Error)
+        #int WebRtcVad_Process(VadInst* handle,
+        #                      int fs,
+        #                      const int16_t* audio_frame,
+        #                      size_t frame_length);
+        lib.WebRtcVad_Process.argtypes = [ctypes.c_void_p, ctypes.c_int, ctypes.POINTER(ctypes.c_int16) , ctypes.c_size_t]
+        lib.WebRtcVad_Process.restype = ctypes.c_int
+        
+        ## Checks for valid combinations of `rate` and `frame_length`. We support 10,
+        ## 20 and 30 ms frames and the rates 8000, 16000 and 32000 Hz.
+        ##
+        ## - rate         [i] : Sampling frequency (Hz).
+        ## - frame_length [i] : Speech frame buffer length in number of samples.
+        ##
+        ## returns            : 0 - (valid combination), -1 - (invalid combination)
+        #int WebRtcVad_ValidRateAndFrameLength(int rate, size_t frame_length);
+        lib.WebRtcVad_ValidRateAndFrameLength.argtypes = [ctypes.c_int, ctypes.c_size_t]
+        lib.WebRtcVad_ValidRateAndFrameLength.restype = ctypes.c_int
+        return lib
 
-## Frees the dynamic memory of a specified VAD instance.
-##
-## - handle [i] : Pointer to VAD instance that should be freed.
-#void WebRtcVad_Free(VadInst* handle);
-lib.WebRtcVad_Free.argtypes = [ctypes.c_void_p]
-lib.WebRtcVad_Free.restype = None
+    _webrtcvad = ffi()
 
-## Initializes a VAD instance.
-##
-## - handle [i/o] : Instance that should be initialized.
-##
-## returns        : 0 - (OK),
-##                 -1 - (null pointer or Default mode could not be set).
-#int WebRtcVad_Init(VadInst* handle);
-lib.WebRtcVad_Init.argtypes = [ctypes.c_void_p]
-lib.WebRtcVad_Init.restype = ctypes.c_int
+    def __init__(self, mode=None):
+        # https://stackoverflow.com/questions/17840144/why-does-setting-ctypes-dll-function-restype-c-void-p-return-long 
+        self.value = self._webrtcvad.WebRtcVad_Create()
+        Vad._webrtcvad.WebRtcVad_Init(self)
+        if mode is not None:
+            self.set_mode(mode)
 
+    def set_mode(self, mode):
+        assert mode in [None, 0, 1, 2]
+        if mode is not None:
+            Vad._webrtcvad.WebRtcVad_set_mode(self, mode)
 
-## Sets the VAD operating mode. A more aggressive (higher mode) VAD is more
-## restrictive in reporting speech. Put in other words the probability of being
-## speech when the VAD returns 1 is increased with increasing mode. As a
-## consequence also the missed detection rate goes up.
-##
-## - handle [i/o] : VAD instance.
-## - mode   [i]   : Aggressiveness mode (0, 1, 2, or 3).
-##
-## returns        : 0 - (OK),
-##                 -1 - (null pointer, mode could not be set or the VAD instance
-##                       has not been initialized).
-#int WebRtcVad_set_mode(VadInst* handle, int mode);
-lib.WebRtcVad_set_mode.argtypes = [ctypes.c_void_p, ctypes.c_int]
-lib.WebRtcVad_set_mode.restype = ctypes.c_int
+    def is_speech(self, buf, sample_rate, length=None):
+        length = length or int(len(buf) / 2)
+        if length * 2 > len(buf):
+            raise IndexError(
+                'buffer has %s frames, but length argument was %s' % (
+                    int(len(buf) / 2.0), length))
+        return Vad._webrtcvad.WebRtcVad_Process(self, sample_rate, buf, length)
 
-## Calculates a VAD decision for the `audio_frame`. For valid sampling rates
-## frame lengths, see the description of WebRtcVad_ValidRatesAndFrameLengths().
-##
-## - handle       [i/o] : VAD Instance. Needs to be initialized by
-##                        WebRtcVad_Init() before call.
-## - fs           [i]   : Sampling frequency (Hz): 8000, 16000, or 32000
-## - audio_frame  [i]   : Audio frame buffer.
-## - frame_length [i]   : Length of audio frame buffer in number of samples.
-##
-## returns              : 1 - (Active Voice),
-##                        0 - (Non-active Voice),
-##                       -1 - (Error)
-#int WebRtcVad_Process(VadInst* handle,
-#                      int fs,
-#                      const int16_t* audio_frame,
-#                      size_t frame_length);
-lib.WebRtcVad_Process.argtypes = [ctypes.c_void_p, ctypes.c_int, ctypes.POINTER(ctypes.c_int16) , ctypes.c_size_t]
-lib.WebRtcVad_Process.restype = ctypes.c_int
+    def __del__(self):
+        Vad._webrtcvad.WebRtcVad_Free(self)
+        self.value = None
 
-## Checks for valid combinations of `rate` and `frame_length`. We support 10,
-## 20 and 30 ms frames and the rates 8000, 16000 and 32000 Hz.
-##
-## - rate         [i] : Sampling frequency (Hz).
-## - frame_length [i] : Speech frame buffer length in number of samples.
-##
-## returns            : 0 - (valid combination), -1 - (invalid combination)
-#int WebRtcVad_ValidRateAndFrameLength(int rate, size_t frame_length);
-lib.WebRtcVad_ValidRateAndFrameLength.argtypes = [ctypes.c_int, ctypes.c_size_t]
-lib.WebRtcVad_ValidRateAndFrameLength.restype = ctypes.c_int
+def valid_rate_and_frame_length(rate, frame_length):
+    return Vad._webrtcvad.WebRtcVad_ValidRateAndFrameLength(rate, frame_length)
+
 
 if __name__ == '__main__':
-    webrtcinst = lib.WebRtcVad_Create()
-    lib.WebRtcVad_Init(webrtcinst)
-    lib.WebRtcVad_Free(webrtcinst)
+    vad = Vad()
+    print(vad)
